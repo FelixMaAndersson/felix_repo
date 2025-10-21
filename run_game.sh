2c0dd993f72bb3942b6e4ef53477759c2a80056d
Felix Andersson <felix.andersson@gmail.com>
2025-10-21 15:30:31 +0200

    first version of the script that runs the game

---
+++ run_game.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+name="Felix_Andersson"
+
+echo "${name}s program"
+mkdir -p "${name}_labb"
+cp ./*.java "./${name}_labb"
+cd ${name}_labb
+pwd
+javac Guesser.java
+javac GuessingGame.java
+echo "Running game..."
+java GuessingGame
+echo "done"
+rm *.class
+ls
